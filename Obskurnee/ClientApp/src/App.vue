89cc8645cf1a5a8d9280ce943078cc7ffe2e8bd2
Hana Stupková <hstupkova@gmail.com>
2021-02-18 02:23:09 +0100

    user edit form styled, css variables added, generic styling for md done

--- Obskurnee/ClientApp/src/App.vue
+++ Obskurnee/ClientApp/src/App.vue
@@ -25,6 +25,23 @@ export default {
 
 <style>
 
+.todo {
+  margin: 2em;
+  color: orangered;
+  text-align: center;
+}
+
+/* variables */
+:root {
+  --spacer: 20px;
+  --c-font: #292929;
+  --c-font-rev: #FFFFFF;
+  --c-accent: #79AA2D;
+  --c-accent-darker: #648d25;
+  --c-accent-secondary: #999999;
+  --c-bckgr-primary: #FFFFFF;
+  --c-bckgr-secondary: #F5F5F5;
+}
 
 html {
   -webkit-box-sizing: border-box;
@@ -38,16 +55,14 @@ html {
   padding: 0;
 }
 
-.todo {
-  margin: 2em;
-  color: orangered;
-  text-align: center;
+body {
+  background-color: var(--c-bckgr-secondary);
 }
 
 #app {
   font-family: 'Raleway', Helvetica, Arial, sans-serif;
   font-size: 100%;
-  color: #292929;
+  color: var(--c-font);
 
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -55,26 +70,108 @@ html {
 }
 
 .page-title {
-  font-size: 2.5em;
+  font-size: 2em;
+  font-weight: bold;
   text-align: center;
-  padding-top: 1em;
-  margin-bottom: 0.8em;
+  margin-top: calc(1.5 * var(--spacer));
+  margin-bottom: var(--spacer);
 }
 
-.button__primary {
-  border: 0;
+.button-primary,
+.button-secondary {
   border-radius: 4px;
-  background-color: #79AA2D;
-  color: white;
-  font-size: 16px;
-  padding: 10px 20px;
+  font-size: 1em;
+  font-weight: bold;
+  padding: 0.625em 1.25em;
   cursor: pointer;
+  display: inline-block;
 }
 
-.button__primary:hover {
-  color: white;
+.button-primary {
+  border: 2px solid var(--c-accent);
+  background-color: var(--c-accent);
+  color: var(--c-font-rev);
+}
+
+.button-primary:hover {
+  color: var(--c-font-rev);
   text-decoration: none;
-  background-color: #648d25;
+  background-color: var(--c-accent-darker);
+  border-color: var(--c-accent-darker);
+}
+
+.button-secondary {
+  border: 2px solid var(--c-accent-secondary);
+  background-color:var(--c-bckgr-primary);
+  color: var(--c-accent-secondary);
+}
+
+.button-secondary:hover {
+  color: var(--c-font-rev);
+  text-decoration: none;
+  background-color: var(--c-accent-secondary);
+}
+
+/* generic styling for markdown */
+p {
+  font-size: 1em;
+  color: var(--c-font);
+  margin-bottom: 1em;
+}
+
+h1, h2, h3 {
+  color: var(--c-font);
+  font-weight: bold;
+}
+
+h1 {
+  font-size: 2em;
+  margin-top: 0.67em;
+  margin-bottom: 0.67em;
+}
+
+h2 {
+  font-size: 1.5em;
+  margin-top: 0.83em;
+  margin-bottom: 0.83em;
+}
+
+h3 {
+  font-size: 1.17em;
+  margin-top: 1em;
+  margin-bottom: 1em;
+}
+
+blockquote {
+  margin-left: var(--spacer);
+  font-style: italic;
+}
+
+ul,
+ol {
+  margin-bottom: 1em;
+  padding-left: calc(2 * var(--spacer));
+}
+
+li {
+  margin-bottom: 0.625em;
+}
+
+img {
+  max-width: 100%;
+}
+
+hr {
+  margin-top: var(--spacer);
+  margin-bottom: var(--spacer);
+}
+
+a {
+  color: var(--c-accent);
+}
+
+a:hover {
+  color: var(--c-accent-darker);
 }
 
 </style>
--- Obskurnee/ClientApp/src/components/pages/User.vue
+++ Obskurnee/ClientApp/src/components/pages/User.vue
@@ -4,6 +4,7 @@
 
     <h1 class="page-title">Uživatelské údaje</h1>
   
+  <!--
   <p v-if="mode == 'edit'">
     <input v-model="user.name" placeholder="meno" /><br />
     <input v-model="user.phone" placeholder="whatsapp telefon" /><br />
@@ -11,6 +12,36 @@
     <textarea v-model="user.aboutMe" placeholder="napis nieco o sebe _(markdown supported)_"></textarea><br />
     <button @click="updateProfile" class="button">Aktualizuj</button>
   </p>
+  -->
+
+  <div v-if="mode == 'edit'">
+    <div class="profile">
+      <div class="form-field">
+        <label for="username" class="label">Jméno:</label>
+        <input type="text" class="input" id="username" v-model="user.name" placeholder="{{ user.name }}" />
+      </div>
+      <div class="form-field">
+        <label for="useremail" class="label">E-mail (nelze změnit):</label>
+        <input type="email" readonly class="input readonly" id="useremail" v-model="user.email" placeholder="{{ user.email }}" />
+      </div>
+      <div class="form-field">
+        <label for="userphone" class="label">Telefon:</label>
+        <input type="tel" class="input" id="userphone" v-model="user.phone" placeholder="{{ user.phone }}" />
+      </div>
+      <div class="form-field">
+        <label for="usergr" class="label">Profil na Goodreads:</label>
+        <input type="url" class="input" id="usergr" v-model="user.goodreadsUrl" placeholder="{{ user.goodreadsUrl }}" />
+      </div>
+      <div class="form-field">
+        <label for="userbio" class="label">Bio:</label>
+        <textarea class="textarea" id="userbio" v-model="user.aboutMe" placeholder="{{ user.aboutMe }}"></textarea>
+      </div>
+    </div>
+    <div class="profile-button">
+      <a @click="updateProfile" class="button-primary button-margin" :v-if="isMod || user.userId == myUserId">Uložit změny</a>
+      <a href="#" class="button-secondary button-margin" :v-if="isMod || user.userId == myUserId">Zahodit změny</a>
+    </div>
+  </div>
 
   <div v-if="mode != 'edit'">
     <!--
@@ -18,39 +49,39 @@
     <a @click="mode = 'edit'" class="button" :v-if="isMod || user.userId == myUserId">EDITUJ</a>
     -->
     <div class="profile">
-      <div class="profile__row">
-        <div class="profile__cat">Jméno:</div>
-        <div class="profile__val">{{ user.name }}</div>
+      <div class="profil-row">
+        <div class="profile-cat">Jméno:</div>
+        <div class="profile-val">{{ user.name }}</div>
       </div>
-      <div class="profile__row">
-        <div class="profile__cat">E-mail:</div>
-        <div class="profile__val">{{ user.email }}</div>
+      <div class="profile-row">
+        <div class="profile-cat">E-mail:</div>
+        <div class="profile-val">{{ user.email }}</div>
       </div>
-      <div class="profile__row">
-        <div class="profile__cat">Telefon:</div>
-        <div class="profile__val">{{ user.phone }}</div>
+      <div class="profile-row">
+        <div class="profile-cat">Telefon:</div>
+        <div class="profile-val">{{ user.phone }}</div>
       </div>
-      <div class="profile__row">
-        <div class="profile__cat">Můj profil na Goodreads:</div>
-        <div class="profile__val">
-          <a href="{{ user.goodreadsUrl }}">
-            {{ user.goodreadsUrl }}
-          </a>
+      <div class="profile-row">
+        <div class="profile-cat">Profil na Goodreads:</div>
+        <div class="profile-val">
+          <a :href="user.goodreadsUrl">{{ user.goodreadsUrl }}</a>
         </div>
       </div>
-      <div class="profile__row">
-        <div class="profile__cat">Bio:</div>
-        <div class="profile__val">{{ user.aboutMe }}</div>
+      <div class="profile-row">
+        <div class="profile-cat">Bio:</div>
+        <div class="profile-val">{{ user.aboutMe }}</div>
       </div>
     </div>
     <div class="profile-button">
-      <a @click="mode = 'edit'" class="button__primary" :v-if="isMod || user.userId == myUserId">Upravit údaje</a>
+      <a @click="mode = 'edit'" class="button-primary" :v-if="isMod || user.userId == myUserId">Upravit údaje</a>
     </div>
 
   </div>
 
 
-  <p class="todo">sem co este? Chceme tu naprikald userove reviews, recs?</p>
+  <p class="todo"><strong>Laci:</strong>sem co este? Chceme tu naprikald userove reviews, recs?</p>
+  <p class="todo"><strong>Rozárka:</strong>To bych nedávala do profilu, ten bych nechala čistě jako přehled osobních údajů určených k editaci. Review a recs bych nechala do sekce My.</p>
+  <p class="todo"><strong>Rozárka:</strong>Budeme sem přidávat profile pic?</p>
   </div>
 </section>
 </template>
@@ -90,27 +121,79 @@ export default {
 
 <style scoped>
 
+  .profile,
+  .profile-button {
+    max-width: 800px;
+  }
+
   .profile {
-    max-width: 600px;
-    background-color: #FFFFFF;
-    margin: 30px auto 30px auto;
-    padding: 40px;
+    background-color: var(--c-bckgr-primary);
+    margin: var(--spacer);
+    padding: calc(2* var(--spacer));
     text-align: left;
   }
 
-  .profile__row {
-    padding: 10px 0 10px 0;
-  }
-
-  .profile__cat {
-    font-weight: bold;
-  }
-
   .profile-button {
-    max-width: 600px;
-    margin: 0 auto;
+    margin: 0 var(--spacer);
     text-align: center;
   }
 
+  @media screen and (min-width: 840px) {
+    .profile {
+      margin: var(--spacer) auto;
+    }
+
+    .profile-button {
+      margin: 0 auto;
+    }
+  }
+
+  .profile-row {
+    padding: calc(0.5 * var(--spacer)) 0;
+  }
+
+  .form-field {
+    margin-bottom: var(--spacer);
+  }
+
+  .profile-cat,
+  .label {
+    font-size: 1.2em;
+    font-weight: bold;
+  }
+
+  .label {
+    display: block;
+  }
+
+  .input {
+    width: 100%;
+    border: 0;
+    background-color: var(--c-bckgr-secondary);
+    padding: 0.5em;
+  }
+
+  .input,
+  .textarea {
+    outline-color: var(--c-accent-secondary);
+  }
+
+  .readonly {
+    outline: none;
+    color: rgba(41, 41, 41, 0.5); /* --c-font plus opacity */
+  }
+
+  .textarea {
+    font-size: 1em;
+    border: 0;
+    background-color: var(--c-bckgr-secondary);
+    padding: 0.5em 1em;
+    width: 100%;
+    height: 15em;
+  }
+
+  .button-margin {
+    margin: calc(0.5 * var(--spacer));
+  }
 
 </style>
