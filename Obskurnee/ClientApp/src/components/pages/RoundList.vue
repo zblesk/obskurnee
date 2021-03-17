98e835c14e800a79f63a4a4aa9d79f90090fd2af
zblesk <b.ladislav@gmail.com>
2021-03-17 05:03:41 +0100

    Collapse form on round add https://zblesk.codecks.io/card/14w-po-pridani-noveho-kola-collapsnut-pridavaci-form

--- Obskurnee/ClientApp/src/components/pages/RoundList.vue
+++ Obskurnee/ClientApp/src/components/pages/RoundList.vue
@@ -108,6 +108,7 @@ export default {
             this.createNewRound(this.newRound).then(() => 
             {
                 this.newRound = {};
+                this.hide = true;
             });
         },
         toggleVisibility()
