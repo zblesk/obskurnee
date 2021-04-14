f6df08f4dfeaac4486b0be60639fa383ea42adbc
zblesk <b.ladislav@gmail.com>
2021-04-14 01:00:27 +0100

    Migrated Polls and Votes

--- Obskurnee/Services/Database.cs
+++ Obskurnee/Services/Database.cs
@@ -12,8 +12,8 @@ namespace Obskurnee.Services
         private readonly LiteDatabase _db;
       //  public readonly ILiteCollection<Post> Posts;
         public readonly ILiteCollection<Book> Books;
-        public readonly ILiteCollection<Poll> Polls;
-        public readonly ILiteCollection<Vote> Votes;
+      //  public readonly ILiteCollection<Poll> Polls;
+      //  public readonly ILiteCollection<Vote> Votes;
         public readonly ILiteCollection<BookclubReview> BookReviews;
         public readonly ILiteCollection<NewsletterSubscription> NewsletterSubscriptions;
 
@@ -25,14 +25,14 @@ namespace Obskurnee.Services
             // Discussions = _db.GetCollection<Discussion>("discussions");
     //        Posts = _db.GetCollection<Post>("posts");
             Books = _db.GetCollection<Book>("books").Include(b => b.Post).Include(b => b.Round);
-            Polls = _db.GetCollection<Poll>("polls").Include(p => p.Options);
-            Votes = _db.GetCollection<Vote>("votes");
+       //     Polls = _db.GetCollection<Poll>("polls").Include(p => p.Options);
+         //   Votes = _db.GetCollection<Vote>("votes");
              // Rounds = _db.GetCollection<Round>("rounds");
             NewsletterSubscriptions = _db.GetCollection<NewsletterSubscription>("newslettersubscriptions");
             BookReviews = _db.GetCollection<BookclubReview>("bookclubreviews").Include(b => b.Book).Include(b => b.Book.Post);
 
     //        Posts.EnsureIndex(p => p.DiscussionId);
-            Votes.EnsureIndex(v => v.PollId);
+     //       Votes.EnsureIndex(v => v.PollId);
             NewsletterSubscriptions.EnsureIndex(ns => ns.UserId);
             NewsletterSubscriptions.EnsureIndex(ns => ns.NewsletterName);
             BookReviews.EnsureIndex(br => br.OwnerId);
