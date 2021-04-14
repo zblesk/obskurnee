a26e137315d7c9c12eaad4f169d77fcf5523e254
zblesk <b.ladislav@gmail.com>
2021-04-14 17:28:19 +0100

    Fixed adding Reviews

--- Obskurnee/Services/ReviewService.cs
+++ Obskurnee/Services/ReviewService.cs
@@ -17,7 +17,7 @@ namespace Obskurnee.Services
         private readonly GoodreadsScraper _scraper;
         private readonly IStringLocalizer<NewsletterStrings> _newsletterLocalizer;
         private readonly NewsletterService _newsletter;
-        private readonly ApplicationDbContext _db2;
+        private readonly ApplicationDbContext _db;
         private readonly Config _config;
 
         public ReviewService(
@@ -26,13 +26,13 @@ namespace Obskurnee.Services
             GoodreadsScraper scraper,
             IStringLocalizer<NewsletterStrings> newsletterLocalizer,
             NewsletterService newsletter,
-            ApplicationDbContext db2)
+            ApplicationDbContext db)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _scraper = scraper ?? throw new ArgumentNullException(nameof(scraper));
             _newsletterLocalizer = newsletterLocalizer ?? throw new ArgumentNullException(nameof(newsletterLocalizer));
             _newsletter = newsletter ?? throw new ArgumentNullException(nameof(newsletter));
-            _db2 = db2 ?? throw new ArgumentNullException(nameof(db2));
+            _db = db ?? throw new ArgumentNullException(nameof(db));
             _config = config ?? throw new ArgumentNullException(nameof(config));
         }
 
@@ -51,30 +51,30 @@ namespace Obskurnee.Services
         //}
 
         public IList<GoodreadsReview> GetCurrentlyReading(string userId) 
-            => _db2.GoodreadsReviews.Where(r => r.OwnerId == userId && r.Kind == CurrentlyReading).ToList();
+            => _db.GoodreadsReviews.Where(r => r.OwnerId == userId && r.Kind == CurrentlyReading).ToList();
 
         public async Task FetchReviewUpdates(Bookworm user)
         {
             try
             {
                 _logger.LogInformation("Updating GR review from shelf RSS for {userId}", user.Id);
-                _db2.GoodreadsReviews.RemoveRange(
-                    _db2.GoodreadsReviews
+                _db.GoodreadsReviews.RemoveRange(
+                    _db.GoodreadsReviews
                         .Where(r => r.OwnerId == user.Id
                                     && r.Kind == CurrentlyReading));
-                await _db2.SaveChangesAsync();
+                await _db.SaveChangesAsync();
                 var reading = _scraper.GetCurrentlyReadingBooks(user).ToList();
                 _logger.LogInformation("Adding {count} Currently Reading for {userId}", reading.Count, user.Id);
-                await _db2.GoodreadsReviews.AddRangeAsync(reading);
+                await _db.GoodreadsReviews.AddRangeAsync(reading);
                 var readBooks = _scraper.GetReadBooks(user).ToList();
                 var ids = readBooks.Select(r => r.ReviewId);
-                var existing = _db2.GoodreadsReviews.Where(r => ids.Contains(r.ReviewId)).ToArray();
+                var existing = _db.GoodreadsReviews.Where(r => ids.Contains(r.ReviewId)).ToArray();
                 _logger.LogInformation("Deleting {count} Read for {userId}", existing.Length, user.Id);
-                _db2.GoodreadsReviews.RemoveRange(existing);
-                await _db2.SaveChangesAsync();
+                _db.GoodreadsReviews.RemoveRange(existing);
+                await _db.SaveChangesAsync();
                 _logger.LogInformation("Adding {count} Read for {userId}", readBooks.Count, user.Id);
-                await _db2.GoodreadsReviews.AddRangeAsync(readBooks);
-                await _db2.SaveChangesAsync();
+                await _db.GoodreadsReviews.AddRangeAsync(readBooks);
+                await _db.SaveChangesAsync();
             }
             catch (Exception ex)
             {
@@ -83,10 +83,10 @@ namespace Obskurnee.Services
         }
 
         public Task<List<BookclubReview>> GetBookReviews(int bookId)
-            => _db2.BookReviewsWithData.Where(br => br.Book.BookId == bookId).ToListAsync();
+            => _db.BookReviewsWithData.Where(br => br.Book.BookId == bookId).ToListAsync();
 
         public Task<List<BookclubReview>> GetUserReviews(string userId)
-            => _db2.BookReviewsWithData.Where(br => br.OwnerId == userId).ToListAsync();
+            => _db.BookReviewsWithData.Where(br => br.OwnerId == userId).ToListAsync();
 
         public async Task<BookclubReview> UpsertBookclubBookReview(
             int bookId,
@@ -95,16 +95,23 @@ namespace Obskurnee.Services
             string reviewText,
             string reviewUrl)
         {
+            var book = await _db.BooksWithData.FirstAsync(b => b.BookId == bookId);
             var review = new BookclubReview(userId)
             {
                 ReviewId = $"{bookId}-{userId}",
-                BookId = bookId,
+                Book = book,
                 ReviewText = reviewText,
                 Rating = rating,
                 ReviewUrl = reviewUrl,
             };
-            await _db2.BookReviews.AddAsync(review);
-            await _db2.SaveChangesAsync();
+            var existing = await _db.BookReviews.FirstOrDefaultAsync(br => br.ReviewId == review.ReviewId);
+            if (existing != null)
+            {
+                _db.BookReviews.Remove(existing);
+                await _db.SaveChangesAsync();
+            }
+            await _db.BookReviews.AddAsync(review);
+            await _db.SaveChangesAsync();
             await SendNewReviewNotification(review);
             return review;
         }
@@ -114,11 +121,13 @@ namespace Obskurnee.Services
             var link = $"{_config.BaseUrl}/knihy/{review.BookId}";
             await _newsletter.SendNewsletter(
                 Newsletters.AllEvents,
-                _newsletterLocalizer.Format("newReviewSubject"),
+                _newsletterLocalizer.Format("newReviewSubject", review.Book.Post.Title),
                 _newsletterLocalizer.FormatAndRender("newReviewBodyMarkdown",
                     link,
+                    Enumerable.Range(0, review.Rating).Aggregate("", (acc, _) => $"{acc}⭐"),
                     review.Rating,
-                    review.ReviewText));
+                    review.ReviewText,
+                    review.OwnerName));
         }
     }
 }
