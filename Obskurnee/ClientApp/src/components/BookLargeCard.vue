691cf961f8680e77887f2840f4b32c8dd4c8748c
Hana Stupková <hstupkova@gmail.com>
2021-04-14 20:04:01 +0100

    BooLargeCard: gr icon added

--- Obskurnee/ClientApp/src/components/BookLargeCard.vue
+++ Obskurnee/ClientApp/src/components/BookLargeCard.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="card">
     <div class="cover">
-      <a :href="post.url" :alt="post.title">
+      <!--<router-link :to="{ name: 'post', params: { postId: post.postId } }">-->
         <img :src="post.imageUrl" :alt="post.title" v-if="post.imageUrl">
-      </a>
+      <!--</router-link>-->
     </div>
+
     <h1 class="title">
-      <a :href="post.url" :alt="post.title">{{ post.author }}: {{ post.title }}</a>
+      <!--<router-link :to="{ name: 'post', params: { postId: post.postId } }">-->{{ post.author }}: {{ post.title }}<!--</router-link>-->
+      <a :href="post.url" :alt="post.title">
+        <span class="goodreads-wrapper">
+          <img src="../assets/goodreads_icon_32x32.png" alt="goodreads icon" class="goodreads-icon">
+        </span>
+      </a>
     </h1>
     <p class="summary" v-html="post.renderedText"></p>
   </div>
@@ -40,6 +46,7 @@
 
   .title {
     font-size: 1.5em;
+    line-height: 1.5;
     font-weight: bold;
     text-align: center;
     margin-top: var(--spacer);
@@ -53,11 +60,24 @@
   @media screen and (min-width: 576px) {
     .title {
       margin-top: 0;
+      text-align: left;
     }
   }
 
   .summary {
     font-size: 1em;
+  }
+
+  .goodreads-wrapper {
+    display: inline-block;
+    width: 1em;
+    height: 1em;
+    margin-left: 0.5em;
+  }
+
+  .goodreads-icon {
+    max-width: 100%;
+    vertical-align: baseline;
   }
 
 </style>
