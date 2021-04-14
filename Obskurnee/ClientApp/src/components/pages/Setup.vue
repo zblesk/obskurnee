e4d4b625a6c2fcd57cbd2e3d89401fbe5c7993d0
Hana Stupková <hstupkova@gmail.com>
2021-04-14 20:27:21 +0100

    Setup page styled

--- Obskurnee/ClientApp/src/components/pages/Setup.vue
+++ Obskurnee/ClientApp/src/components/pages/Setup.vue
@@ -1,17 +1,16 @@
 <template>
 <div>
-  <form @submit.prevent="onSubmit">
-      <input id="emailInput"
-              type="email"
-              v-model="form.email"
-              required
-              placeholder="tvoj@mail" />
-      <input id="passwordInput"
-                    type="password"
-                    v-model="form.password"
-                    required
-                    placeholder="h3sl0" />
-    <button class="btn btn-primary ml-2" type="submit">Zaregistrovat</button>
+  <h1 class="page-title">Vytvoření nového uživatele</h1>
+  <form @submit.prevent="onSubmit" class="main">
+    <div class="form-field">
+      <label for="emailInput">E-mailová adresa</label>
+      <input id="emailInput" type="email" v-model="form.email" required>
+    </div>
+    <div class="form-field">
+      <label for="passwordInput">Heslo</label>
+      <input id="passwordInput" type="password" v-model="form.password" required>
+    </div>
+    <button class="button-primary" type="submit">Zaregistrovat</button>
   </form>
 </div>
 </template>
@@ -43,3 +42,39 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+  .main {
+    padding: var(--spacer);
+    background-color: var(--c-bckgr-primary);
+    max-width: 800px;
+    margin: 0 var(--spacer) calc(var(--spacer) * 2) var(--spacer);
+  }
+
+  @media screen and (min-width: 840px) {
+    .main {
+      margin: 0 auto var(--spacer) auto;
+    }
+  }
+
+  .form-field {
+    margin-bottom: var(--spacer);
+  }
+
+  .form-field label {
+    display: block;
+    margin-bottom: calc(var(--spacer) / 2);
+  }
+
+  .form-field textarea {
+    width: 100%;
+    height: 15em;
+    margin-bottom: calc(var(--spacer) / 2);
+  }
+
+  .form-field input {
+    width: 100%;
+    max-width: 380px;
+    margin-bottom: calc(var(--spacer) / 2);
+  }
+</style>
