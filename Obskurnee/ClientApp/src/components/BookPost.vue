38fc43a281f9538aa88acc2b02e4d5b7097e8291
zblesk <b.ladislav@gmail.com>
2021-04-22 04:58:57 +0100

    Book post updated to include author

--- Obskurnee/ClientApp/src/components/BookPost.vue
+++ Obskurnee/ClientApp/src/components/BookPost.vue
@@ -6,12 +6,14 @@
         </a>
       </div>
       <a :href="post.url" class="book__link">
-        <h2 class="book__title">{{ post.title }}</h2></a> 
-          <router-link v-if="isRepostingAllowed && activeDiscussionId != post.discussionId" 
-            :to="{ name: 'discussion', params: { discussionId: activeDiscussionId }, 
-            query: { fromPostId: post.postId, fromDiscussionId: post.discussionId } }">
-              ⬆⬆⬆
-          </router-link>
+        <h2 class="book__title">{{ post.title }}</h2>
+      </a> 
+      <p v-if="post.author">{{ post.author }}</p>
+      <router-link v-if="isRepostingAllowed && activeDiscussionId != post.discussionId" 
+        :to="{ name: 'discussion', params: { discussionId: activeDiscussionId }, 
+        query: { fromPostId: post.postId, fromDiscussionId: post.discussionId } }">
+          ⬆⬆⬆
+      </router-link>
       <p class="book__pages" v-if="post.pageCount">{{ post.pageCount }} strán</p>
       <p class="book__owner">Navrhla {{ post.ownerName }}</p>
       <div class="book__text" v-html="post.renderedText"> </div>
