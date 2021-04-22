59315c23aa94822a06526339d5798aaec886938f
zblesk <b.ladislav@gmail.com>
2021-04-22 04:46:07 +0100

    Formatting rec card name

--- Obskurnee/ClientApp/src/components/RecommendationCard.vue
+++ Obskurnee/ClientApp/src/components/RecommendationCard.vue
@@ -6,8 +6,9 @@
       </a>
     </div>
     <a :href="recommendation.url" class="book__link">
-      <h2 class="book__title">{{recommendation.author}}: {{ recommendation.title }}</h2>
+      <h2 class="book__title">{{ recommendation.title }}</h2>
     </a>
+    <p v-if="recommendation.author">{{ recommendation.author }}</p>
     <p class="book__pages" v-if="recommendation.pageCount">{{ recommendation.pageCount }} strán</p>
     <p class="book__owner" v-if="showName">Doporučila {{ recommendation.ownerName }}</p>
     <p class="book__text" v-html="recommendation.renderedText"> </p>
