67348776262e16d3151375e8b35758c395007c2f
Hana Stupková <hstupkova@gmail.com>
2021-04-25 19:37:06 +0100

    NewPost buttons position fixed

--- Obskurnee/ClientApp/src/components/NewPost.vue
+++ Obskurnee/ClientApp/src/components/NewPost.vue
@@ -3,57 +3,59 @@
   <div v-if="hide" class="button-show-wrapper">
     <button @click="toggleVisibility" class="button-primary button-show"><slot></slot></button>
   </div>
-  <div v-else class="form">
-    <div class="cover">
-      <img :src="newPost.imageUrl" v-if="newPost.imageUrl" :alt="newPost.title" />
-    </div>
-    <div class="form-text">
-      <div v-if="mode != 'Themes'" class="form-field">
-        <label for="grlink">Odkaz na Goodreads</label>
-        <div class="note u-mb-sp05">
-          <div class="note-pic">
-              <img src="../assets/lamp.svg" alt="lamp icon" />
-          </div>
-          <p class="note-text">Vlož odkaz, zmáčkni enter nebo tabulátor a chvíli počkej.</p>
-        </div>
-        <input v-model="newPost.url" placeholder="https://www.goodreads.com/book..." @change="linkChange" id="grlink" />
-        <p v-if="fetchInProgress" class="alert-inline">Kamo počkaj, LOADUJEM</p>
-      </div>
-      <div class="cover-mobile">
+  <div v-else>
+    <div class="form">
+      <div class="cover">
         <img :src="newPost.imageUrl" v-if="newPost.imageUrl" :alt="newPost.title" />
       </div>
-      <div class="form-field">
-        <label for="name" v-if="mode == 'Books' || mode == 'Recommendations'">Název knihy*</label>
-        <label for="name" v-if="mode == 'Themes'">Název tématu*</label>
-        <input v-model="newPost.title" id="name" required />
-      </div>
-      <div class="form-field" v-if="mode != 'Themes'">
-        <label for="author">Jméno autora*</label>
-        <input v-model="newPost.author" id="autor" required />
-      </div>
-      <div class="form-field" v-if="mode != 'Themes'">
-        <label for="pages">Počet stran*</label>
-        <input type="number" v-model="newPost.pageCount" id="pages" required />
-      </div>
-      <div class="form-field u-mb-0">
-        <div class="label-md-wrapper">
-          <label for="text">Komentár k návrhu*</label>
-          <div class="mo-md">
-            <div class="mo-md-pic">
-              <img src="../assets/Markdown-mark.svg" alt="markdown logo">
+      <div class="form-text">
+        <div v-if="mode != 'Themes'" class="form-field">
+          <label for="grlink">Odkaz na Goodreads</label>
+          <div class="note u-mb-sp05">
+            <div class="note-pic">
+                <img src="../assets/lamp.svg" alt="lamp icon" />
             </div>
-            <div class="mo-md-link">
-              <markdown-help-link></markdown-help-link>
+            <p class="note-text">Vlož odkaz, zmáčkni enter nebo tabulátor a chvíli počkej.</p>
+          </div>
+          <input v-model="newPost.url" placeholder="https://www.goodreads.com/book..." @change="linkChange" id="grlink" />
+          <p v-if="fetchInProgress" class="alert-inline">Kamo počkaj, LOADUJEM</p>
+        </div>
+        <div class="cover-mobile">
+          <img :src="newPost.imageUrl" v-if="newPost.imageUrl" :alt="newPost.title" />
+        </div>
+        <div class="form-field">
+          <label for="name" v-if="mode == 'Books' || mode == 'Recommendations'">Název knihy*</label>
+          <label for="name" v-if="mode == 'Themes'">Název tématu*</label>
+          <input v-model="newPost.title" id="name" required />
+        </div>
+        <div class="form-field" v-if="mode != 'Themes'">
+          <label for="author">Jméno autora*</label>
+          <input v-model="newPost.author" id="autor" required />
+        </div>
+        <div class="form-field" v-if="mode != 'Themes'">
+          <label for="pages">Počet stran*</label>
+          <input type="number" v-model="newPost.pageCount" id="pages" required />
+        </div>
+        <div class="form-field u-mb-0">
+          <div class="label-md-wrapper">
+            <label for="text">Komentár k návrhu*</label>
+            <div class="mo-md">
+              <div class="mo-md-pic">
+                <img src="../assets/Markdown-mark.svg" alt="markdown logo">
+              </div>
+              <div class="mo-md-link">
+                <markdown-help-link></markdown-help-link>
+              </div>
             </div>
           </div>
+          <textarea v-model="newPost.text" id="text" required :placeholder="$t('global.markdownSamplePlaceholder')"></textarea>
         </div>
-        <textarea v-model="newPost.text" id="text" required :placeholder="$t('global.markdownSamplePlaceholder')"></textarea>
+        <p class="asterisk">Pole označená * jsou povinná.</p>
       </div>
-      <p class="asterisk">Pole označená * jsou povinná.</p>
-      <div class="buttons">
-        <button @click="addPost" class="button-primary">Přidat</button>
-        <button @click="toggleVisibility" class="button-secondary hide-form">Schovat formulář</button>
-      </div>
+    </div>
+    <div class="buttons u-mt-sp">
+      <button @click="addPost" class="button-primary">Přidat</button>
+      <button @click="toggleVisibility" class="button-secondary hide-form">Schovat formulář</button>
     </div>
   </div>
 </div>
