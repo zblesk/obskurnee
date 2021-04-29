f6d9a7e8738006e9848e91925595eb942e718619
zblesk <b.ladislav@gmail.com>
2021-04-29 21:44:21 +0100

    NewPost localized

--- Obskurnee/ClientApp/src/components/NewPost.vue
+++ Obskurnee/ClientApp/src/components/NewPost.vue
@@ -10,35 +10,35 @@
       </div>
       <div class="form-text">
         <div v-if="mode != 'Themes'" class="form-field">
-          <label for="grlink">Odkaz na Goodreads</label>
+          <label for="grlink">{{$t('newpost.grLink')}}</label>
           <div class="note u-mb-sp05">
             <div class="note-pic">
                 <img src="../assets/lamp.svg" alt="lamp icon" />
             </div>
-            <p class="note-text">Vlož odkaz, zmáčkni enter nebo tabulátor a chvíli počkej.</p>
+            <p class="note-text">{{$t('newpost.grLoadInstructions')}}</p>
           </div>
           <input v-model="newPost.url" placeholder="https://www.goodreads.com/book..." @change="linkChange" id="grlink" />
-          <p v-if="fetchInProgress" class="alert-inline">Kamo počkaj, LOADUJEM</p>
+          <p v-if="fetchInProgress" class="alert-inline">{{$t('newpost.waitForLoadPlox')}}</p>
         </div>
         <div class="cover-mobile">
           <img :src="newPost.imageUrl" v-if="newPost.imageUrl" :alt="newPost.title" />
         </div>
         <div class="form-field">
-          <label for="name" v-if="mode == 'Books' || mode == 'Recommendations'">Název knihy*</label>
-          <label for="name" v-if="mode == 'Themes'">Název tématu*</label>
+          <label for="name" v-if="mode == 'Books' || mode == 'Recommendations'">{{$t('newpost.bookTitle')}}*</label>
+          <label for="name" v-if="mode == 'Themes'">{{$t('newpost.topicTitle')}}*</label>
           <input v-model="newPost.title" id="name" required />
         </div>
         <div class="form-field" v-if="mode != 'Themes'">
-          <label for="author">Jméno autora*</label>
+          <label for="author">{{$t('newpost.authorName')}}*</label>
           <input v-model="newPost.author" id="autor" required />
         </div>
         <div class="form-field" v-if="mode != 'Themes'">
-          <label for="pages">Počet stran*</label>
+          <label for="pages">{{$t('newpost.pageCount')}}*</label>
           <input type="number" v-model="newPost.pageCount" id="pages" required />
         </div>
         <div class="form-field u-mb-0">
           <div class="label-md-wrapper">
-            <label for="text">Komentár k návrhu*</label>
+            <label for="text">{{$t('newpost.comment')}}*</label>
             <div class="mo-md">
               <div class="mo-md-pic">
                 <img src="../assets/Markdown-mark.svg" alt="markdown logo">
@@ -50,12 +50,12 @@
           </div>
           <textarea v-model="newPost.text" id="text" required :placeholder="$t('global.markdownSamplePlaceholder')"></textarea>
         </div>
-        <p class="asterisk">Pole označená * jsou povinná.</p>
+        <p class="asterisk">{{$t('newpost.requiredFields')}}</p>
       </div>
     </div>
     <div class="buttons u-mt-sp">
-      <button @click="addPost" :disabled="saveInProgress" class="button-primary">Přidat</button>
-      <button @click="toggleVisibility" class="button-secondary hide-form">Schovat formulář</button>
+      <button @click="addPost" :disabled="saveInProgress" class="button-primary">{{$t('menus.add')}}</button>
+      <button @click="toggleVisibility" class="button-secondary hide-form">{{$t('newpost.hideForm')}}</button>
     </div>
   </div>
 </div>
