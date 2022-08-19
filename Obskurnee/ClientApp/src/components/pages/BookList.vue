f012cb4f7392b750ce9338a3ae6d9bcacc9165e2
zblesk <ladislav.benc@gmail.com>
2022-08-19 22:26:52 +0200

    Added placeholder when the Books page is empty

--- Obskurnee/ClientApp/src/components/pages/BookList.vue
+++ Obskurnee/ClientApp/src/components/pages/BookList.vue
@@ -1,21 +1,27 @@
 <template>
 <section>
     <h1 class="page-title">{{$t('menus.books')}}</h1>
-    <div class="books-grid" v-if="books">
+    <div class="books-grid" v-if="books?.length > 0">
         <book-preview v-bind:book="book" v-for="book in books" v-bind:key="book.bookId">#{{ book.order }} </book-preview>
+    </div>
+    <div class="placeholder" v-else>
+        <span>{{$t('booklist.noBooksYet')}}</span>
     </div>
 </section>
 </template>
 
 <style scoped>
+.books-grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+    gap: var(--spacer);
+    padding: var(--spacer);
+}
 
-    .books-grid {
-        display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-        gap: var(--spacer);
-        padding: var(--spacer);
-    }
-
+.placeholder {
+  text-align: center;
+  font-style: italic;
+}
 </style>
 
 <script>
@@ -23,15 +29,15 @@ import { mapActions, mapState } from "vuex";
 import BookPreview from '../BookPreview.vue';
 export default {
   components: { BookPreview },
-name: "BookList",
-computed: {
-    ...mapState("books", ["books"]),
-},
-methods: {
-...mapActions("books", ["fetchBookList"]),
-},
-mounted() {
+  name: "BookList",
+  computed: {
+  ...mapState("books", ["books"]),
+  },
+  methods: {
+  ...mapActions("books", ["fetchBookList"]),
+  },
+  mounted() {
     this.fetchBookList();
-}
+  }
 }
 </script>
