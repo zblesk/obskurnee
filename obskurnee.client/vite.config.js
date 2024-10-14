1e2d88bc7dad9771ae94304729fbffdf294b4718
zblesk <ladislav.benc@gmail.com>
2024-10-14 21:01:14 +0200

    Removed unused HTTPS config code which started breaking builds recently.

--- obskurnee.client/vite.config.js
+++ obskurnee.client/vite.config.js
@@ -7,31 +7,8 @@ import path from 'path';
 import child_process from 'child_process';
 import { env } from 'process';
 
-const baseFolder =
-    env.APPDATA !== undefined && env.APPDATA !== ''
-        ? `${env.APPDATA}/ASP.NET/https`
-        : `${env.HOME}/.aspnet/https`;
 
-const certificateName = "obskurnee.client";
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
-
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
-    }
-}
-
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:5151';
+const target = env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:5151';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -57,9 +34,5 @@ export default defineConfig({
             },
         },
         port: 5284
-    //https: {
-    //    key: fs.readFileSync(keyFilePath),
-    //    cert: fs.readFileSync(certFilePath),
-    //}
     }
 })
